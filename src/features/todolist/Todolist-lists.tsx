f40c81d492119d49b8ce7fa92e2ff27e76f8d96b
Maxim Dmitriev <Renissance@tut.by>
2023-04-25 12:14:31 +0300

    refactor

--- src/features/todolist/Todolist-lists.tsx
+++ src/features/todolist/Todolist-lists.tsx
@@ -1,7 +1,10 @@
 import Grid from '@mui/material/Grid/Grid'
 import { AddItemForm } from 'common/comonents/AddItemForm'
 import React, { useCallback, useEffect } from 'react'
-import { TodolistDomainType, todolistThunks } from 'features/todolist/Todolist/todolists-reducer'
+import {
+	TodolistDomainType,
+	todolistThunks
+} from 'features/todolist/Todolist/todolists-reducer'
 import { useAppSelector } from 'common/hooks/useApp'
 import { selectTodolists } from 'features/todolist/Todolist/todolists-selectors'
 import { selectIsLoggedIn } from 'features/auth/auth-selectors'
@@ -13,51 +16,51 @@ import { TasksType } from 'features/todolist/Todolist/Task/tasks-reducer'
 import { useActions } from 'common/hooks/useActions'
 
 export const TodolistLists = () => {
-  const { addTodoList, getTodoList } = useActions(todolistThunks)
-  const todoLists: Array<TodolistDomainType> = useAppSelector(selectTodolists)
-  const tasks: TasksType = useAppSelector(selectTasks)
-  const isLoggedIn = useAppSelector(selectIsLoggedIn)
+	const { addTodoList, getTodoList } = useActions(todolistThunks)
+	const todoLists: Array<TodolistDomainType> = useAppSelector(selectTodolists)
+	const tasks: TasksType = useAppSelector(selectTasks)
+	const isLoggedIn = useAppSelector(selectIsLoggedIn)
 
-  const addTodoListCallback = useCallback((title: string) => {
-    addTodoList(title)
-  }, [])
+	const addTodoListCallback = useCallback((title: string) => {
+		addTodoList(title)
+	}, [])
 
-  useEffect(() => {
-    if (!isLoggedIn) {
-      return
-    }
-    getTodoList()
-  }, [])
+	useEffect(() => {
+		if (!isLoggedIn) {
+			return
+		}
+		getTodoList()
+	}, [])
 
-  if (!isLoggedIn) {
-    return <Navigate to='/login' />
-  }
+	if (!isLoggedIn) {
+		return <Navigate to='/login' />
+	}
 
-  return (
-    <>
-      <Grid container style={{ paddingTop: '10px' }}>
-        <AddItemForm callBack={addTodoListCallback} />
-      </Grid>
-      <Grid container spacing={3}>
-        {todoLists.map((el) => {
-          let tasksForRender = tasks[el.id]
+	return (
+		<>
+			<Grid container style={{ paddingTop: '10px' }}>
+				<AddItemForm callBack={addTodoListCallback} />
+			</Grid>
+			<Grid container spacing={3}>
+				{todoLists.map(el => {
+					let tasksForRender = tasks[el.id]
 
-          return (
-            <Grid item key={el.id}>
-              <Paper style={{ padding: '10px' }}>
-                <Todolist
-                  key={el.id}
-                  todolistId={el.id}
-                  filter={el.filter}
-                  title={el.title}
-                  entityStatus={el.entityStatus}
-                  tasks={tasksForRender}
-                />
-              </Paper>
-            </Grid>
-          )
-        })}
-      </Grid>
-    </>
-  )
+					return (
+						<Grid item key={el.id}>
+							<Paper style={{ padding: '10px' }}>
+								<Todolist
+									key={el.id}
+									todolistId={el.id}
+									filter={el.filter}
+									title={el.title}
+									entityStatus={el.entityStatus}
+									tasks={tasksForRender}
+								/>
+							</Paper>
+						</Grid>
+					)
+				})}
+			</Grid>
+		</>
+	)
 }
